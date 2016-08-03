5af4470c47c51708d820dac7ae0eb3f97d69f2ea
TPPcragain <chris.ragain@taxplannerpro.com>
2016-08-03 16:14:34 +0000

    sendgrid update host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   
-  config.action_mailer.default_url_options = { :host => 'yourapp.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'calm-reaches-25080.heroku.com' }
     
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
